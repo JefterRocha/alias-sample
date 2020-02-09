69d433f58208213f853a2afc04a1d6d311c4905f
Jefter Rocha <32802064+JefterRocha@users.noreply.github.com>
2020-02-09 19:20:23 -0300

    Update moduleAliasConfig.js

--- config/moduleAliasConfig.js
+++ config/moduleAliasConfig.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
 	'@root': path.join(__dirname, '..'),
-	'@controllers': path.join(__dirname, '..', './controllers'),
-	'@routes': path.join(__dirname, '..', './routes')
+	'@controllers': path.join(__dirname, '..', 'controllers'),
+	'@routes': path.join(__dirname, '..', 'routes')
 }
